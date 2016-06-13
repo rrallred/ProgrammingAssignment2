11735572c765cec3c38137f72595d268695f3049
i58471 <rrallred@xactware.com>
2016-06-12 22:47:23 -0600

    Caching Inverse Matrix - Reid Allred

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Returns chached inversed matrix value, if already exists uses cached value
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix: provides the original matrix and the functions to be consumed and executed by cacheSolve
+makeCacheMatrix <- function(x = matrix()) 
+{
+  getmatrix<-function() x
+  getinverse <- function() im_globe
+  setinverse <- function(im) im_globe <<- im
+  list(getmatrix=getmatrix, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve: determines if cached inversed matrix exist, if not it returns the inversed matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  if(exists("im_globe"))
+  {  im <- x$getinverse() }
+  else {im_globe<-NULL}
+  if(!is.null(im_globe)) {
+    message("getting cached data")
+    return(im_globe)
+  }
+  data <- x$getmatrix()
+  im <- solve(data, ...)
+  x$setinverse(im)
+  im
 }
